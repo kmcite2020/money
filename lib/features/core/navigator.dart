47da3433fb6fc63409534b0ae8eea0d601478f87
kmcite2020 <lumia.college.learning.458@gmail.com>
2023-11-20 21:42:02 +0500

    update

---
+++ lib/features/core/navigator.dart
@@ -0,0 +1,27 @@
+import 'package:states_rebuilder/states_rebuilder.dart';
+
+final navigator = RM.navigate;
+
+// final navigator = RM.injectNavigator(
+//   routes: {
+//     Routes.home: (data) => HomePage(),
+//     Routes.settings: (data) => SettingsPage(),
+//     Routes.peoples: (data) => PeoplesPage(),
+//     Routes.people: (data) => PeoplePage(id: data.arguments),
+//     Routes.addPeople: (data) => AddPeoplePage(),
+//     Routes.entries: (data) => EntriesScreen(),
+//     Routes.entry: (data) => EntryScreen(id: data.arguments),
+//     Routes.addEntry: (data) => AddEntryScreen(),
+//   },
+// );
+//
+// abstract class Routes {
+//   static const home = '/';
+//   static const settings = '/settings';
+//   static const peoples = '/peoples';
+//   static const entries = '/entries';
+//   static const people = '/people';
+//   static const entry = '/entry';
+//   static const addPeople = '/add_people';
+//   static const addEntry = '/add_entry';
+// }
