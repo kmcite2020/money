719e269338f01e63e0f2946ff941bc03e5471175
kmcite2020 <lumia.college.learning.458@gmail.com>
2024-01-29 14:08:14 +0500

    update

--- lib/features/persons/pages/person_page.dart
+++ lib/features/persons/pages/person_page.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import '../../../main.dart';
 
 class PersonPage extends UI {
@@ -10,73 +11,39 @@ class PersonPage extends UI {
     return PersonBuilder(
       id: id,
       builder: (Person person) {
-        final transactions = transactionsManager.getTransactionsByPersonID(id);
-        transactions.sort(
-          (a, b) => a.created.compareTo(b.created),
-        );
         return Scaffold(
-          appBar: AppBar(
-            actions: [
-              IconButton(
-                onPressed: () {
-                  personsManager
-                      .addPerson(person.copyWith(editing: !person.editing));
-                },
-                icon: Icon(person.editing ? Icons.done : Icons.edit),
-              ),
-            ],
-          ),
+          appBar: AppBar(),
           body: Column(
             children: [
-              'ID'.text(textScaleFactor: 2).pad(),
-              person.personID.text().center(),
-              if (person.editing)
-                TextFormField(
-                  initialValue: person.name,
-                  onChanged: (value) {
-                    personsManager.addPerson(
-                      person.copyWith(name: value),
-                    );
-                  },
-                  onFieldSubmitted: (value) {
-                    personsManager.addPerson(
-                      person.copyWith(editing: !person.editing),
-                    );
-                  },
-                  decoration: InputDecoration(labelText: 'NAME'),
-                ).pad()
-              else ...[
-                'NAME'.text(textScaleFactor: 2),
-                person.name.text(textScaleFactor: 1.5).pad(),
-              ],
-              'AMOUNT'.text(textScaleFactor: 2),
-              person.amount.text(textScaleFactor: 1.5).pad(),
-              IconButton.outlined(
+              person.personID.text(textScaleFactor: 4),
+              person.name.text(textScaleFactor: 5),
+              TextFormField(
+                initialValue: person.name,
+                onChanged: (value) {
+                  personsManager.addPerson(person.copyWith(name: value));
+                },
+                onFieldSubmitted: (value) {},
+              ).pad(),
+              IconButton(
                 onPressed: () {
-                  transactionsManager.addTransaction(
-                    Transaction().copyWith(personID: id),
-                  );
+                  // transactionsManager.addTransaction(
+                  //   Transaction.create().copyWith(personID: id),
+                  // );
                 },
                 icon: Icon(
                   Icons.add_a_photo_sharp,
                 ),
               ),
-              Wrap(
-                children: transactions
-                    .map(
-                      (e) => InputChip(
-                        onPressed: () {
-                          navigator.to(
-                            TransactionPage(id: e.transactionID),
-                          );
-                        },
-                        label: e.amount.text(),
-                      ).pad(
-                        customPadding: EdgeInsets.all(4),
-                      ),
-                    )
-                    .toList(),
-              )
+              // Wrap(
+              //   children: transactionsManager
+              //       .getTransactionByPersonID(id)
+              //       .map(
+              //         (e) => Chip(
+              //           label: e.amount.text(),
+              //         ),
+              //       )
+              //       .toList(),
+              // )
             ],
           ),
         );
--- lib/features/transactions/pages/transaction_page.dart
+++ lib/features/transactions/pages/transaction_page.dart
@@ -1,7 +1,10 @@
 import '../../../main.dart';
 
 class TransactionPage extends UI {
-  const TransactionPage({required this.id});
+  const TransactionPage({
+    Key? key,
+    required this.id,
+  }) : super(key: key);
   final String id;
   @override
   Widget build(BuildContext context) {
@@ -11,133 +14,52 @@ class TransactionPage extends UI {
         return Scaffold(
           appBar: AppBar(
             title: transaction.person.name.text(),
-            backgroundColor: transaction.isPersonValid
-                ? settingsManager.settings.materialColor
-                : Colors.red,
-            actions: [
-              IconButton(
-                onPressed: () async {
-                  await Future.delayed(17.milliseconds);
-                  transactionsManager
-                      .removeTransaction(transaction.transactionID);
-                },
-                icon: Icon(Icons.delete),
-              ),
-              IconButton(
-                onPressed: () {
-                  transactionsManager.addTransaction(
-                    transaction.copyWith(editing: !transaction.editing),
-                  );
-                },
-                icon: Icon(transaction.editing ? Icons.done : Icons.edit),
-              ).pad(
-                customPadding: EdgeInsets.only(
-                  right: settingsManager.settings.padding,
-                ),
-              )
-            ],
+            // backgroundColor:
+            //     transaction.isPersonInvalid ? Colors.red : Colors.amber,
           ),
           body: ListView(
             children: [
-              'DATED'.text(textScaleFactor: 2).pad(),
-              transaction.created.datetime().text().pad(),
-              Divider(height: 0),
               'Transaction ID'.text(textScaleFactor: 2).pad(),
-              transaction.transactionID.text().pad(),
+              transaction.transactionID.text(textScaleFactor: 3).pad(),
               Divider(height: 0),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   'Person'.text(textScaleFactor: 2).pad(),
-                  Row(
-                    children: [
-                      IconButton(
-                        tooltip: 'Goto person details',
-                        onPressed: transaction.isPersonValid
-                            ? () {
-                                navigator.to(
-                                  PersonPage(id: transaction.personID!),
-                                );
-                              }
-                            : null,
-                        icon: Icon(Icons.read_more),
-                      ).pad(),
-                      PopupMenuButton(
-                        enabled: personsManager.persons.isNotEmpty,
-                        itemBuilder: (_) {
-                          return personsManager.persons
-                              .map(
-                                (eachPerson) => PopupMenuItem(
-                                  enabled: eachPerson.personID !=
-                                      transaction.personID,
-                                  value: eachPerson.personID,
-                                  child: eachPerson.name.text(),
-                                ),
-                              )
-                              .toList();
-                        },
-                        onSelected: (personID) {
-                          transactionsManager.addTransaction(
-                            transaction.copyWith(personID: personID),
-                          );
-                        },
-                      ).pad(
-                        customPadding: EdgeInsets.only(
-                          right: settingsManager.settings.padding,
-                        ),
-                      ),
-                    ],
+                  PopupMenuButton(
+                    itemBuilder: (_) {
+                      return personsManager.persons
+                          .map(
+                            (e) => PopupMenuItem(
+                              value: e.personID,
+                              child: e.text(),
+                            ),
+                          )
+                          .toList();
+                    },
+                    onSelected: (value) {
+                      print(value);
+                      transactionsManager.addTransaction(
+                        transaction.copyWith(transactionID: value),
+                      );
+                    },
                   ),
+                  Icon(Icons.done).pad(),
                 ],
               ),
-              transaction.person.name.text(textScaleFactor: 3).pad(),
+              // ElevatedButton(
+              //   onPressed: transaction.isPersonInvalid
+              //       ? null
+              //       : () {
+              //           navigator.to(PersonPage(id: transaction.personID));
+              //         },
+              //   child: transaction.person.text(textScaleFactor: 3).pad(),
+              // ),
               Divider(height: 0),
-              Row(
-                children: [
-                  'Amount'.text(textScaleFactor: 2).pad(),
-                ],
-              ),
-              if (transaction.editing)
-                TextFormField(
-                  initialValue: transaction.amount.toString(),
-                  onChanged: (value) {
-                    final val = int.tryParse(value);
-                    if (val == null) return;
-                    transactionsManager.addTransaction(
-                      transaction.copyWith(amount: val),
-                    );
-                  },
-                  validator: (value) {
-                    final val = int.tryParse(value!);
-                    if (val == null) return 'Please enter valid number';
-                    return null;
-                  },
-                  keyboardType: TextInputType.number,
-                  autovalidateMode: AutovalidateMode.always,
-                ).pad()
-              else
-                transaction.amount.text(textScaleFactor: 3).pad(),
+              'Amount'.text(textScaleFactor: 2).pad(),
+              transaction.amount.text(textScaleFactor: 3).pad(),
               Divider(height: 0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  'Transaction Notes'.text(textScaleFactor: 2).pad(),
-                ],
-              ),
-              if (transaction.editing)
-                TextFormField(
-                  initialValue: transaction.notes,
-                  minLines: 5,
-                  maxLines: 10,
-                  onChanged: (notes) {
-                    transactionsManager.addTransaction(
-                      transaction.copyWith(notes: notes),
-                    );
-                  },
-                  autovalidateMode: AutovalidateMode.always,
-                ).pad()
-              else
-                transaction.notes.text(textScaleFactor: 3).pad()
+              'Transaction Notes'.text(textScaleFactor: 2).pad(),
+              transaction.notes.text(textScaleFactor: 3).pad(),
             ],
           ),
         );
